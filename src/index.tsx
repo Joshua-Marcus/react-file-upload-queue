19de5e7f0fcc4bbdcff92e6df3a8d21dbad7a296
Josh Marcus <contact@joshuamarcus.com.au>
2022-12-20 11:07:56 +1030

    minor refactor + remove xstate

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import ReactUpload from './pages/react-upload/ReactUpload.page';
+import ReactUpload from './pages/upload/ReactUpload.page';
 import {
   Routes,
   Route,
   BrowserRouter as Router,
   Navigate,
 } from 'react-router-dom';
-import StateXUpload from './pages/xstate-upload/xStateUpload.page';
 
 ReactDOM.render(
   <React.StrictMode>
     <div className='m-0 p-0 w-screen h-screen'>
       <Router>
         <Routes>
-          <Route path='/' element={<Navigate to='/react-upload' />} />
-          <Route path='/react-upload' element={<ReactUpload />} />
-          <Route path='/xstate-upload' element={<StateXUpload />} />
+          <Route path='/' element={<Navigate to='/upload' />} />
+          <Route path='/upload' element={<ReactUpload />} />
         </Routes>
       </Router>
     </div>
